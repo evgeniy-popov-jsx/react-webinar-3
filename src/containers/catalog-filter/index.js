d080294998b38319320de9c6025288bb5094646b
Evgeniy Popov <99990148+JENYAODERZHIMIY@users.noreply.github.com>
2023-12-13 14:39:40 +0300

    фильтрация по категориям

--- src/containers/catalog-filter/index.js
+++ src/containers/catalog-filter/index.js
@@ -16,9 +16,12 @@ function CatalogFilter() {
   const select = useSelector(state => ({
     sort: state.catalog.params.sort,
     query: state.catalog.params.query,
+    category: state.catalog.params.category,
   }));
 
   const callbacks = {
+    // Фильтрация
+    onCategory: useCallback(category => store.actions.catalog.setParams({category, page: 1}), [store]),
     // Сортировка
     onSort: useCallback(sort => store.actions.catalog.setParams({sort}), [store]),
     // Поиск
@@ -33,13 +36,27 @@ function CatalogFilter() {
       {value: 'title.ru', title: 'По именованию'},
       {value: '-price', title: 'Сначала дорогие'},
       {value: 'edition', title: 'Древние'},
-    ]), [])
+    ]), []),
+    category: useMemo(() => ([
+      {value: "", title: 'Все'},
+      { value: "65782dc9cbf638685992a338", title: "Электроника" },
+      { value: "65782dc9cbf638685992a339", title: "- Телефоны" },
+      { value: "65782dc9cbf638685992a340", title: "-- Смартфоны" },
+      { value: "65782dc9cbf638685992a341", title: "-- Аксессуары" },
+      { value: "65782dc9cbf638685992a33a", title: "- Ноутбуки" },
+      { value: "65782dc9cbf638685992a33b", title: "- Телевизоры" },
+      { value: "65782dc9cbf638685992a33c", title: "Книги" },
+      { value: "65782dc9cbf638685992a33d", title: "- Учебники" },
+      { value: "65782dc9cbf638685992a33e", title: "- Художественная" },
+      { value: "65782dc9cbf638685992a33f", title: "- Комиксы" },
+    ]), []),
   };
 
   const {t} = useTranslate();
 
   return (
     <SideLayout padding='medium'>
+      <Select options={options.category} value={select.category} onChange={callbacks.onCategory}/>
       <Select options={options.sort} value={select.sort} onChange={callbacks.onSort}/>
       <Input value={select.query} onChange={callbacks.onSearch} placeholder={'Поиск'}
              delay={1000}/>
